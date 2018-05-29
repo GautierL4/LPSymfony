de53212dc4bc7924262e82428075fa74df4144fe
GautierL4 <gautier.laurent4@gmail.com>
2018-05-29 20:23:34 +0200

    Set up user bundle

---
+++ src/GL/HomeBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace GL\HomeBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
+
